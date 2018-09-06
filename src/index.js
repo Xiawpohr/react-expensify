50fc1f540db1577ca08368f8740c07b431a09a29
XiawPohr <paradise121329@gmail.com>
2018-09-06 10:53:42 +0800

    npm install react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import './index.css'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const Dashboard = () => <div>Dashboard page</div>
+const Create = () => <div>create page</div>
+const Edit = () => <div>edit page</div>
+const Help = () => <div>help page</div>
+const NotFoundPage = () => <div>404</div>
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' component={Dashboard} exact />
+      <Route path='/create' component={Create} />
+      <Route path='/edit' component={Edit} />
+      <Route path='/help' component={Help} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('root'))
+registerServiceWorker()
