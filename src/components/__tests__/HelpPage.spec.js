9299060edceb402831abd8aec13cd4021ac6bfb0
XiawPohr <paradise121329@gmail.com>
2018-09-16 01:07:48 +0800

    update snapshots

--- src/components/__tests__/HelpPage.spec.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import HelpPage from '../HelpPage.js'
-
-test('should render help page correctly', () => {
-  const wrapper = shallow(<HelpPage />)
-  expect(wrapper).toMatchSnapshot()
-})
